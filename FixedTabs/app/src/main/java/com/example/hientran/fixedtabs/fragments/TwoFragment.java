a0c38e64609259c35e2c0add5a8691158d1abf26
trantronghienit <you@example.com>
2016-07-15 13:51:02 +0700

    first add

---
+++ FixedTabs/app/src/main/java/com/example/hientran/fixedtabs/fragments/TwoFragment.java
@@ -0,0 +1,31 @@
+package com.example.hientran.fixedtabs.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.hientran.fixedtabs.R;
+
+/**
+ * Created by HienTran on 7/15/2016.
+ */
+public class TwoFragment extends Fragment {
+
+    public TwoFragment(){
+
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_two , container , false);
+    }
+}
