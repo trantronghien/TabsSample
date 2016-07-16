f0f844093d5e5d55bbcfda7396813536c46706b8
trantronghienit <you@example.com>
2016-07-16 12:53:26 +0700

    Tabs Icon and custom tabs

---
+++ Tabs_Icon/app/src/main/java/com/example/hientran/tabs_icon/Fragments/OneFragment.java
@@ -0,0 +1,32 @@
+package com.example.hientran.tabs_icon.Fragments;
+
+/**
+ * Created by HienTran on 7/15/2016.
+ */
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.hientran.tabs_icon.R;
+
+
+public class OneFragment extends Fragment{
+
+    public OneFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_one, container, false);
+    }
+}
